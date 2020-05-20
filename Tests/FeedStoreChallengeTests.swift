43ed78e36119b0a79991e761ed3195e5d06ccec8
pandit.pakhurde <pandit.pakhurde@quikr.com>
2020-05-20 11:09:02 +0530

    Retrieving from empty cache delivers empty results

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -5,6 +5,22 @@
 import XCTest
 import FeedStoreChallenge
 
+class InMemoryFeedStore: FeedStore {
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+    }
+    
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+    
+}
+
 class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	
 //
@@ -14,9 +30,9 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 //
 
 	func test_retrieve_deliversEmptyOnEmptyCache() {
-//		let sut = makeSUT()
-//
-//		assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+		let sut = makeSUT()
+
+		assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
 	}
 
 	func test_retrieve_hasNoSideEffectsOnEmptyCache() {
@@ -88,7 +104,8 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	// - MARK: Helpers
 	
 	private func makeSUT() -> FeedStore {
-		fatalError("Must be implemented")
+        let sut = InMemoryFeedStore()
+        return sut
 	}
 	
 }
